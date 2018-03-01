0a3cd72ee294c76233b64ecaf88b1054a7f5caac
Modestas <modestask97@gmail.com>
2018-03-01 21:08:09 +0000

    created login and reigister form and functionality components as well aswell as static authentication methods

--- src/app.js
+++ src/app.js
@@ -2,13 +2,22 @@ import React    from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Link, Route } from 'react-router-dom';
 
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+
 import './scss/style.scss';
 
 class App extends React.Component {
 
   render() {
     return (
-      <h1>Euphoria</h1>
+      <BrowserRouter>
+        <main>
+          <h1>Euphoria</h1>
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+        </main>
+      </BrowserRouter>
     );
   }
 }
