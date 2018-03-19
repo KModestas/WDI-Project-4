bcadfcee34cac7b39e6f65da03f3aa4da575ac64
Modestas <modestask97@gmail.com>
2018-03-19 12:34:38 +0000

    changed gig untrack backend code

--- controllers/users.js
+++ controllers/users.js
@@ -16,17 +16,24 @@ function favouriteGig(req, res, next) {
 }
 
 function unTrackGig(req, res, next) {
+
+  const gig = req.body;
+
   User
     .findById(req.currentUser.id)
     .then(user => {
       if (!user) return res.notFound();
 
-      user.gigs.map( gig => {
-        if (gig.id === req.body) {
-          user.gigs.splice(gig);
-        }
-        return user.save();
-      });
+      const index = user.gigs.indexOf(gig);
+      user.gigs.splice(index, 1);
+
+      return user.gigs.save();
+      // user.gigs.map( gig => {
+      //   if (gig.id === req.body) {
+      //     user.gigs.splice(gig);
+      //   }
+      //   return user.save();
+      // });
     })
     .then(user => {
       res.json(user);
