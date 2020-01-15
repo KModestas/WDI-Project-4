fa07f0bb065609b028ce9edcb3795ef112160feb
Modestas <modestask97@gmail.com>
2020-01-15 15:13:21 +0000

    homepage change

--- src/components/other/HomePage.js
+++ src/components/other/HomePage.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-
-const AboutPage =()=> {
-  return(
+const AboutPage = () => {
+  return (
     <div>
       <video src="../assets/concert.mov" autoPlay="true" loop="true" />
       <div className="overlay">
         <div className="container">
           <div className="row justify-content-center align-items-center">
             <div className="aboutText col-sm-12 col-md-12 col-lg-8">
-              <p>Euphoria  is the ultimate webpage for keeping track of your favourite, most anticipated upcoming gigs in London. Register an account, search for a gig and click the track button. Are you ready?</p>
-              <Link to="/gigs" className="proceed">Hell Yeah!</Link>
+              <p>
+                Euphoria is the ultimate web app for tracking upcoming gigs in
+                London. Are you ready?
+              </p>
+              <Link to="/gigs" className="proceed">
+                Let's go!
+              </Link>
             </div>
           </div>
         </div>
